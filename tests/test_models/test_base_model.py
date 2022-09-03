10d37e5084279ca30f4c4665184acb510f65e4a8
Chris-ade <joeade12054@gmail.com>
2022-09-03 10:29:23 -0700

    Update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,8 +11,4 @@ my_model_json = my_model.to_dict()
 print(my_model_json)
 print("JSON of my_model:")
 for key in my_model_json.keys():
-<<<<<<< HEAD
     print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
-=======
-    print("\t{}: ({}) - {}".format(key,type(my_model_json[key]),my_model_json[key]))
->>>>>>> d371fc684d780557007d78f8471c9a9326f91b59
